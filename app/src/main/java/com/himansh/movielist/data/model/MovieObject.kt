ebceea5bac5ab1bc118c6c736c15b053b68cd0c7
himanshuPatel <informhimanshupatel@gmail.com>
2021-01-23 13:08:14 -0500

    Implement Movie Info screen using data binding

--- app/src/main/java/com/himansh/movielist/data/model/MovieObject.kt
+++ app/src/main/java/com/himansh/movielist/data/model/MovieObject.kt
@@ -1,3 +1,29 @@
 package com.himansh.movielist.data.model
 
-data class MovieObject(var Title: String, var Year: String, var imdbID: String, var Type: String, var Poster: String)
+data class MovieObject(var Title: String,
+                       var Year: String,
+                       var Rated: String,
+                       var Released: String,
+                       var Runtime: String,
+                       var Genre: String,
+                       var Director: String,
+                       var Writer: String,
+                       var Actors: String,
+                       var Plot: String,
+                       var Language: String,
+                       var Country: String,
+                       var Awards: String,
+                       var Poster: String,
+                       var Ratings: ArrayList<Rating>,
+                       var Metascore: String,
+                       var imdbRating: String,
+                       var imdbVote: String,
+                       var imdbID: String,
+                       var DVD: String,
+                       var BoxOffice: String,
+                       var Production: String,
+                       var Website: String,
+                       var Response: String,
+                       var Type: String)
+
+data class Rating(val Source: String, val Value: String)
--- app/src/main/java/com/himansh/movielist/ui/MovieInfo.kt
+++ app/src/main/java/com/himansh/movielist/ui/MovieInfo.kt
@@ -1,71 +1,73 @@
 package com.himansh.movielist.ui
 
-import android.app.ProgressDialog
+
 import android.os.Bundle
-import android.widget.TextView
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.himansh.movielist.R
+import com.himansh.movielist.data.model.MovieObject
+import com.himansh.movielist.data.rest.RepoService
+import com.himansh.movielist.databinding.ActivityMovieInfoBinding
+import com.himansh.movielist.util.RetrofitClientInstance
+import com.squareup.picasso.Picasso
+import retrofit2.Call
+import retrofit2.Callback
 
 class MovieInfo : AppCompatActivity() {
-    var Title: TextView? = null
-    var Released: TextView? = null
-    var Rating: TextView? = null
-    var Genre: TextView? = null
-    var Director: TextView? = null
-    var Writer: TextView? = null
-    var Actor: TextView? = null
-    var Plot: TextView? = null
-    //var Poster: NetworkImageView? = null
-    private var pDialog: ProgressDialog? = null
-    private var url = "http://www.omdbapi.com/?i=tt0099785&apikey=94a221d"
-    //var imageLoader: ImageLoader? = AppController.getInstance().getImageLoader()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_info)
-        //Poster = findViewById(R.id.imageViewPoster)
-        url = "http://www.omdbapi.com/?i=" + intent.getStringExtra("imdbID") + "&apikey=94a221d"
-        Title = findViewById(R.id.textViewTitle)
-        Released = findViewById(R.id.textViewReleased)
-        Rating = findViewById(R.id.textViewIMDBRatings)
-        Genre = findViewById(R.id.textViewGenre)
-        Director = findViewById(R.id.textViewDirector)
-        Writer = findViewById(R.id.textViewWriter)
-        Actor = findViewById(R.id.textViewActors)
-        Plot = findViewById(R.id.textViewPlot)
-        pDialog = ProgressDialog(this)
-        pDialog!!.setMessage("Loading...")
-        pDialog!!.show()
-       /* val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null, { response ->
-            hidePDialog()
-            try {
-                Title.setText(response.getString("Title"))
-                Released.setText(response.getString("Released"))
-                Genre.setText("Genre: " + response.getString("Genre"))
-                Director.setText("Director: " + response.getString("Director"))
-                Writer.setText("Writer: " + response.getString("Writer"))
-                Actor.setText("Cast: " + response.getString("Actors"))
-                Plot.setText("""
-    Plot:
-    ${response.getString("Plot")}
-    """.trimIndent())
-                Rating.setText(response.getString("imdbRating") + "/10")
-                if (imageLoader == null) imageLoader = AppController.getInstance().getImageLoader()
-                val thumbNail = findViewById<View>(R.id.imageViewPoster) as NetworkImageView
-                thumbNail.setImageUrl(response.getString("Poster"), imageLoader)
-            } catch (e: JSONException) {
-                e.printStackTrace()
-            }
-        }) { error ->
-            showError(error)
-            hidePDialog()
-        }*/
-        //AppController.getInstance().addToRequestQueue(jsonObjectRequest)
+
+    companion object {
+        private const val API_KEY = "94a221d"
     }
 
-    /*private fun showError(error: VolleyError) {
+    private var pDialog: ProgressDialog? = null
+    private lateinit var repoService: RepoService
+    private lateinit var binding: ActivityMovieInfoBinding
+
+    private lateinit var movieID: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_movie_info)
+        movieID = intent.getStringExtra("imdbID").toString()
+
+        repoService = RetrofitClientInstance.retrofitInstance.create(RepoService::class.java)
+        ProgressDialog.initialise(this)
+
+        getMovieDetails()
+
+    }
+
+    private fun getMovieDetails() {
+
+        ProgressDialog.show()
+
+        val searchMovieCall: Call<MovieObject> = repoService.getMovieDetail(movieID, API_KEY)
+        searchMovieCall.enqueue(object : Callback<MovieObject?> {
+            override fun onResponse(call: Call<MovieObject?>?, response: retrofit2.Response<MovieObject?>) {
+                ProgressDialog.dismiss()
+                Log.d("TAG", response.code().toString() + "")
+                val resource: MovieObject = response.body()!!
+                binding.movieObject = resource
+
+                Picasso.get()
+                        .load(resource.Poster)
+                        .into(binding.moviePoster)
+            }
+
+            override fun onFailure(call: Call<MovieObject?>, t: Throwable) {
+                call.cancel()
+                showError(t.message!!)
+                ProgressDialog.dismiss()
+            }
+        })
+    }
+
+    private fun showError(error: String) {
         Toast.makeText(this, "I'll handle this Error if you hire me!\n\n$error", Toast.LENGTH_LONG).show()
-    }*/
+    }
 
     public override fun onDestroy() {
         super.onDestroy()
