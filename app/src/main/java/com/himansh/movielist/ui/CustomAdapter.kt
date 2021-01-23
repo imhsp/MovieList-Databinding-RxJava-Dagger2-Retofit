17fedd8e411961e45c13c8cc90418fab0a472226
himanshuPatel <informhimanshupatel@gmail.com>
2021-01-23 12:40:40 -0500

    Add images to Movie List

--- app/src/main/java/com/himansh/movielist/ui/CustomAdapter.kt
+++ app/src/main/java/com/himansh/movielist/ui/CustomAdapter.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.himansh.movielist.R
 import com.himansh.movielist.data.model.MovieObject
+import com.squareup.picasso.Picasso
 
 
 class CustomAdapter(private val context: Context, private var movieList: ArrayList<MovieObject>, var onItemClick: (position: Int) -> Unit) : RecyclerView.Adapter<CustomAdapter.MovieObjectViewHolder>() {
@@ -27,11 +29,17 @@ class CustomAdapter(private val context: Context, private var movieList: ArrayLi
 
     class MovieObjectViewHolder(itemView: View, var onItemClick: (position: Int) -> Unit) : RecyclerView.ViewHolder(itemView) {
 
+        private val moviePoster: ImageView = itemView.findViewById(R.id.moviePoster)
         private val movieTitle: TextView = itemView.findViewById(R.id.movieName)
         private val movieType: TextView = itemView.findViewById(R.id.movieType)
         private val movieYear: TextView = itemView.findViewById(R.id.movieYear)
 
         fun bind(movieObject: MovieObject, position: Int) {
+
+            Picasso.get()
+                    .load(movieObject.Poster)
+                    .into(moviePoster)
+
             movieTitle.text = movieObject.Title
             movieType.text = movieObject.Type
             movieYear.text = movieObject.Year
